fe30888e2e6a25ac9143eb9f23dc662c7651ace5
phptangem <phptangem@163.com>
2017-04-07 21:12:18 +0800

    c20170407

--- admin.php
+++ admin.php
@@ -17,6 +17,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
+//缓存目录设置
+define('RUNTIME_PATH', './Runtime/');
+
 //开发模式环境变量前缀
 define('ENV_PRE', 'TEM_');
 
